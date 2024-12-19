b31437520b7f20bb97351d5d37df8032a992dbaf
ShwetaC21 <163808310+surajanireysoft@users.noreply.github.com>
2024-12-19 23:43:18 +0530

    Initial commit for BookMyParking project

---
+++ app/src/main/java/com/example/bookmyparking/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.bookmyparking
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signinsignup)
+
+        val buttonSignIn: Button = findViewById(R.id.btnSignIn)
+        val buttonSignUp: Button = findViewById(R.id.btnSignUp)
+
+        buttonSignIn.setOnClickListener {
+        }
+
+        buttonSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
