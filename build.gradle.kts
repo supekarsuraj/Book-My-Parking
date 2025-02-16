2dd4f332e99e06ef945bfb45ae41ce2814fd4d9a
supekarsuraj <136548699+supekarsuraj@users.noreply.github.com>
2025-02-16 13:17:50 +0530

    join project to firebase
    join project to firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10") // Update here
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
