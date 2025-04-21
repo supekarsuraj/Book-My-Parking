dd7920204b00be1422b89cb7502e53fd4caf1ff4
supekarsuraj <136548699+supekarsuraj@users.noreply.github.com>
2025-04-21 07:57:17 +0530

    add-toggle
    add-toggle

---
+++ app/src/main/java/com/example/bookmyparking/ImportantArrays/ImpArrays.kt
@@ -0,0 +1,7 @@
+package com.example.bookmyparking.ImportantArrays
+
+class ImpArrays {
+
+    var beutyFlwlessSkin1="this is my layout but in this layout when i click Read more that time i want to show  Enim lectus mauris faucibus turpis convallis ipsum odio lorem dignissim id odio sed enim nullam venenatis erat in cursus tortor tristique aliquam nulla quis."
+    var beutyFlwlessSkin2="Aliquam scelerisque scelerisque ac laoreet laoreet faucibus vestibulum fringilla ut aliquet dictumst dignissim id eget cras nulla venenatis dictum tristique faucibus lectus egestas aenean placerat dolor dolor phasellus eros vestibulum velit turpis ornare facilisis ante ornare."
+}
